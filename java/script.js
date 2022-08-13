dc8f937dd0b1013b89047f7c0546dbb93959bbf3
Higor de Faria <higorpr@gmail.com>
2022-08-13 20:53:29 -0300

    Finishing modifications to select function Adapting html

--- java/script.js
+++ java/script.js
@@ -1,9 +1,24 @@
+let ok_list = [false, false, false] // Control variable for unlocking button
 
-function pickMeal(box, target_check) {
+function pickBox(box, target_check) {
+    // Defining which section the click took place:    
+    if (box.parentElement.classList.contains('meal') === true) {
+        ok_list[0] = true
+        section = '.meal'
+    } else if (box.parentElement.classList.contains('drink') === true) {
+        ok_list[1] = true
+        section = '.drink'
+    } else {
+        ok_list[2] = true
+        section = '.dessert'
+    }
+    picked_box = `${section} .picked`
+    picked_checkmark = `${section} .checkmark`
 
-    last_picked_box = document.querySelector('.picked')
-    last_picked_checkmark = document.querySelector('.checkmark')
-    checkmark = document.querySelector(target_check)
+    const last_picked_box = document.querySelector(picked_box)
+    const last_picked_checkmark = document.querySelector(picked_checkmark)
+    const checkmark = document.querySelector(target_check)
+    
 
     if (last_picked_box === null) { // First click
         console.log('PRIMEIRO CLIQUE')
@@ -12,14 +27,15 @@ function pickMeal(box, target_check) {
         box.classList.toggle('picked')
         checkmark.classList.toggle('hide')
         checkmark.classList.toggle('checkmark')
-
+        
     } else if (last_picked_box === box) { // Double click on same box:
-        console.log('CLIQUE NA MESMA CAIXA')
-        console.log(last_picked_box)
-        console.log(box)
-        box.classList.toggle('picked')
-        checkmark.classList.toggle('hide')
-        checkmark.classList.toggle('checkmark')
+        // alert('Você já selecionou esse item =)')
+        // console.log('CLIQUE NA MESMA CAIXA')
+        // console.log(last_picked_box)
+        // console.log(box)
+        // box.classList.toggle('picked')
+        // checkmark.classList.toggle('hide')
+        // checkmark.classList.toggle('checkmark')
 
     } else { // Click on different box
         console.log('CLIQUE EM CAIXA DIFERENTE')
@@ -32,12 +48,11 @@ function pickMeal(box, target_check) {
         checkmark.classList.toggle('checkmark')
         checkmark.classList.toggle('hide')
     }
+
+    okOrder()
 }
 
-function pickDrink() {
-
-}
-
-function pickDessert() {
-
+function okOrder () {
+    texto = document.querySelector('.footer p')
+    console.log(texto)
 }
