e0b9dd6d744c30cf57715e2bde3e81617a29adcb
Higor de Faria <higorpr@gmail.com>
2022-08-12 23:54:09 -0300

    Completed Base HTML and CSS / Implementing JS order selection code

---
+++ java/script.js
@@ -0,0 +1,16 @@
+function pickMeal(target_box, target_check) {
+    
+    box = document.querySelector(target_box)
+    check =document.querySelector(target_check)
+    console.log(box)
+    console.log(check)
+    // box.classList.add('picked')
+}
+
+function pickDrink() {
+    
+}
+
+function pickDessert() {
+    
+}
